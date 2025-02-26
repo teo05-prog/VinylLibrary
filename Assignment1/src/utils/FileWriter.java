46986ed3ef2ba9d474300ad3c122d785ac1be111
Teo <teodora.cristiana.stoicescu@gmail.com>
2025-02-26 13:39:32 +0100

    added states
    Took 17 minutes
    
    
    Took 8 seconds

--- Assignment1/src/utils/FileWriter.java
+++ Assignment1/src/utils/FileWriter.java
@@ -41,7 +41,7 @@ public class FileWriter
       vinyls[0] = new Vinyl("Thriller", "Michael Jackson", 1982, "available");
       vinyls[1] = new Vinyl("Abbey - Road", "Beatles", 1969, "available");
       vinyls[2] = new Vinyl("Greatest Hits - 2", "Queen", 1981, "available");
-      vinyls[3] = new Vinyl("Purple Rain", "Price", 1984, "available");
+      vinyls[3] = new Vinyl("Purple Rain", "Prince", 1984, "available");
       vinyls[4] = new Vinyl("Master of Puppets", "Metallica", 1986, "available");
       vinyls[5] = new Vinyl("Curtain Call: The Hits", "Eminem", 2005, "available");
       vinyls[6] = new Vinyl("Back in Black", "AC/DC", 2003, "available");
